bb9e90951441717362331ff7541ecb15cad2087b
hlb <1394105227@qq.com>
2019-05-15 23:31:13 +0800

    获取文件服务器数据，存入作用域

--- product-view-service-feign/src/main/java/com/hlbbxh/product_view_service_feign/controller/ProductController.java
+++ product-view-service-feign/src/main/java/com/hlbbxh/product_view_service_feign/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.hlbbxh.product_view_service_feign.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,19 @@ import com.hlbbxh.product_view_service_feign.service.ProductService;
 
 @Controller
 public class ProductController {
+	
     @Autowired ProductService productService;
+    
+    //获取配置服务器的配置信息
+    @Value("${version}")
+    String version;
+    
     @RequestMapping("/products")
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
         m.addAttribute("ps", ps);
+        //放入作用域
+        m.addAttribute("version", version);
         return "products";
     }
 }
